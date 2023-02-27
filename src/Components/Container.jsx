132e5c8d9b7dd44b6050023353d3cccbd4bca72a
OmarCovolo <OmarCovolo@users.noreply.github.com>
2023-02-27 16:16:11 +0100

    hjhuk

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -11,11 +11,8 @@ class Container extends React.Component {
         return (
         <div className="container-fluid px-4">
             <ContentBetween/>
-            <h4>Trending Now</h4>
             <Trending search="hobbit"/>
-            <h4>Watch it again</h4>
             <Again search="Evil Dead"/>
-            <h4>New Releases</h4>
             <New search="Star Wars"/>
             <Footer/>
         </div>
